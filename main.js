08579e7ae586822d0e97116efcda5968a11e65db
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-02-28 02:26:03 -0300

    projeto finalizado

--- main.js
+++ main.js
@@ -1,40 +1,68 @@
-const cardTitleQuotes = document.querySelectorAll('.quotes .cards .card-title');
-const cardValuesQuotes = document.querySelectorAll('.quotes .cards .card-value');
+const cardTitleQuotes = document.querySelectorAll(".quotes .cards .card-title");
+const cardValuesQuotes = document.querySelectorAll(
+  ".quotes .cards .card-value"
+);
 
-const cardTitleStocks = document.querySelectorAll('.stocks .cards .card-title');
-const cardValuesStocks = document.querySelectorAll('.stocks .cards .card-value');
+const cardTitleStocks = document.querySelectorAll(".stocks .cards .card-title");
+const cardValuesStocks = document.querySelectorAll(
+  ".stocks .cards .card-value"
+);
 
-const production = 'https://api.hgbrasil.com/finance?key=c38d9f1a'
-const local = 'https://cors-everywhere.onrender.com/https://api.hgbrasil.com/finance?key=c38d9f1a'
+function fetchData() {
+  fetch(
+    `https://cors-everywhere.onrender.com/https://api.hgbrasil.com/finance?key=c38d9f1a`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const results = data.results;
+      const stocks = results.stocks;
+      const currencies = results.currencies;
 
-fetch(`${local}`)
-  .then(response => response.json())
-  .then(data => {
-    const results = data.results;
-    const stocks = results.stocks;
-    const currencies = results.currencies;
-
-    console.log(results)
-
-    Object.entries(stocks).map(([key, value], index) => {
-      [...cardTitleStocks].map
-        (element => {
+      Object.entries(stocks).map(([key, value], index) => {
+        [...cardTitleStocks].map((element) => {
           const id = element.dataset.id;
           if (id === key) {
-            cardTitleStocks[index].textContent = value.name;
-            cardValuesStocks[index].textContent = value.variation + '%';
+            value.name === "Índice de Fundos de Investimentos Imobiliários B3"
+              ? (cardTitleStocks[1].innerHTML = "IFIX")
+              : (cardTitleStocks[index].textContent = value.name);
+
+            cardValuesStocks[index].textContent = value.variation + "%";
           }
         });
-    })
+      });
 
-    Object.entries(currencies).map(([key, value], index) => {
-      [...cardTitleQuotes].map
-        (element => {
+      Object.entries(currencies).map(([key, value], index) => {
+        [...cardTitleQuotes].map((element) => {
           const id = element.dataset.id;
           if (id === key) {
-            cardTitleQuotes[index].textContent = value.name;
-            cardValuesQuotes[index].textContent = value.buy;
+            value.name === "Renminbi"
+              ? (cardTitleQuotes[7].innerHTML = "Chinese Yuan")
+              : cardTitleQuotes[index - 1].textContent = value.name;
+
+            cardValuesQuotes[index - 1].textContent = value.buy.toLocaleString(
+              "pt-BR",
+              { style: "currency", currency: "BRL" }
+            );
           }
         });
-    })
-  })
+      });
+    });
+}
+
+// Display current date and time
+function displayDate() {
+  const dateHourElem = document.getElementById("date-hour");
+  const now = new Date();
+  const formattedDate = now.toLocaleString("pt-BR", { dateStyle: "short" });
+  const formattedTime = now.toLocaleString("pt-BR", { timeStyle: "short" });
+  dateHourElem.textContent = `Atualizado em: ${formattedDate} às ${formattedTime}`;
+}
+
+// Call fetchData() immediately
+fetchData();
+
+// Call fetchData() every hour (in milliseconds)
+setInterval(fetchData, 3600000);
+
+// Update the date and time display every second
+setInterval(displayDate, 1000);
